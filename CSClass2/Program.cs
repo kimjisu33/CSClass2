70f4bc6eb739d6dd8c8f7ab267e00c475eb333b6
kimjisu33 <indexkim33@gmail.com>
2022-08-19 10:49:48 +0900

    #2 Indexer 문법

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -10,6 +10,7 @@ namespace CSClass2
     {
         static void Main(string[] args)
         {
+            //제네릭
             Wanted<string> wantedString = new Wanted<string>("String");
             Wanted<int> wantedInt = new Wanted<int>(1111);
             Wanted<double> wantedDouble = new Wanted<double>(22.2222);
@@ -17,6 +18,13 @@ namespace CSClass2
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
+
+
+            //인덱서
+            Products p = new Products();
+            Console.WriteLine("오늘의 점심 메뉴는 " + p[0] + "입니다.");
+            p[0] = "우동";
+            Console.WriteLine("오늘의 점심 메뉴는 " + p[0] + "입니다.");
         }
     }
 }
